5226279a6006b0b05f12562beea1575804dd0679
tcaseria10 <tcaseria19@my.whitworth.edu>
2017-03-03 07:04:09 -0800

    Update Playlist.cpp

--- CS172-Exam1/Playlist.cpp
+++ CS172-Exam1/Playlist.cpp
@@ -6,33 +6,46 @@
 // constructor for the playlist
 Playlist::Playlist(std::string name) {
 	myname = name;
-}
-	;
+};
 
 // get the name of the Playlist
-std::string getName();
+std::string Playlist::getName() {
+	return Playlist::myname;
+};
 
 // returns true when at least one song of 
 // this genre is present.
 bool Playlist::hasGenre(std::string genre) {
-	if (Playlist)
+	int itdoeshavegenre=0;
+	for (int counter = 1; counter <= Playlist::numSongs; counter++) {
+		if (songArray[counter].Song::mygenre == genre)
+			itdoeshavegenre++;
+			return true;
+	}
+		return false;
 };
 
 // Add a new song
 void Playlist::addSong(Song song) {
+	int songcount = 0;
+	int counter = 0;
 	if (numSongs < MAX_SONGS) {
-		songArray[numberOfSongs];
-		numSongs++;
+		Playlist::songArray[counter] = song;
+		songcount++;
+		counter++;
+		numSongs = songcount;
 	}
-
-
 };
 
 // How many songs in this playlist?
 int Playlist::numberOfSongs() {
-
+	return Playlist::numSongs;
 };
 
 // Play a song.  return an empty string if there is no such song.
 std::string Playlist::playSong(int songNumber) {
+	if (songArray[songNumber].mygenre!="")
+		return "PLAYING!";
+	else
+		return "";
 };
