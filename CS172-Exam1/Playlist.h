479268499616d4cfe7828e7c3cdbd02443845a2c
tcaseria10 <tcaseria19@my.whitworth.edu>
2017-03-03 07:04:32 -0800

    Update Playlist.h

--- CS172-Exam1/Playlist.h
+++ CS172-Exam1/Playlist.h
@@ -9,15 +9,16 @@
 class Playlist {
 private:
 	std::string myname;
-	const int MAX_SONGS = 1000;
+	const int MAX_SONGS = 999;
 	int numSongs;
-	Song songArray[];
+	Song songArray[1000];
 public:
 	Playlist(std::string name);
 	bool hasGenre(std::string genre);
 	void addSong(Song song);
 	std::string playSong(int songNumber);
 	int numberOfSongs();
+	std::string getName();
 }; 
 #endif
 #pragma once
