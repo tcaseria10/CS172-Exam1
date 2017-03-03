cfcf218f35976434024f857727be373df38a5a55
tcaseria10 <tcaseria19@my.whitworth.edu>
2017-03-03 07:04:56 -0800

    Update Song.cpp

--- CS172-Exam1/Song.cpp
+++ CS172-Exam1/Song.cpp
@@ -2,10 +2,8 @@
 #include <string>
 
 
-// default song
-Song::Song() {
-	mytitle = "notitle"
-};
+
+
 // allow the caller to set up the song.  
 // valid genres are "ModernPop", "Grunge", "Classical" and "Americana".
 Song::Song(std::string title, std::string artist, std::string genre) {
@@ -14,19 +12,24 @@ Song::Song(std::string title, std::string artist, std::string genre) {
 	mygenre = genre;
 };
 
-std::string getTitle() const {
+// default song
+Song::Song() {
+	mytitle = "notitle";
+};
 
+std::string Song::getTitle()  const {
+	return mytitle;
 };  // return the song title
-std::string getArtist() const {
-
+std::string Song::getArtist() const {
+	return myartist;
 }; // return the artist who performed the song
-std::string getGenre() const {
-
+std::string Song::getGenre() const {
+	return mygenre;
 };  // return the genre of the song
 
 						  // return true if the song is of the genre.
 bool Song::isOfGenre(std::string genre) {
-	if (Song.genre == genre)
+	if (mygenre == genre)
 		return true;
 	else
 		return false;
